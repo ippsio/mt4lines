7d5422ed36c43b63f40b15dad3ea6f83084635ad
hoge <hoge>
2017-04-25 11:05:15 +0900

    SYMBOL[]

--- cmd-gitpush.bat
+++ cmd-gitpush.bat
@@ -1,6 +1,5 @@
 echo off
-call :main %* 2>>%0
-pause
+call :main %*
 goto :eof
 :main
 pause
@@ -8,7 +7,7 @@ echo #########################
 echo # GIT ADD, COMMIT, PUSH
 echo #########################
 setlocal
-echo "hoehogehoge" > hoge.txt
+
 set GIT_EXE=git
 set SYMBOL=SYMBOL[%1]
 echo CURRENT DIRECTORY=[%~dp0]
@@ -28,6 +27,3 @@ echo [push]---------------------------
 echo finish.
 
 endlocal
-pauseWarning: Permanently added the RSA host key for IP address '192.30.255.112' to the list of known hosts.
-From github.com:ippsio/mt4lines
- * branch            master     -> FETCH_HEAD
